1dece2d1b5aa86a56b6d849fde86b7b077f66a1f
temless <ljw980722@naver.com>
2025-01-14 18:05:02 +0900

    s

--- lib/screen/myPage.dart
+++ lib/screen/myPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lastdance_f/model/student_model.dart';
+import 'package:lastdance_f/screen/myPageUpdate.dart';
 
 class Mypage extends StatefulWidget {
   const Mypage({super.key});
@@ -8,11 +10,381 @@ class Mypage extends StatefulWidget {
 }
 
 class _MypageState extends State<Mypage> {
+  final int studentId = 1; // 학생 ID
+  Map<String, dynamic> _student = {};
+  final StudentModel _studentModel = StudentModel();
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _loadStudent(); // 데이터 로드
+  }
+
+  void _loadStudent() async {
+    try {
+      Map<String, dynamic> studentData = await _studentModel.searchDetailStudent(studentId);
+      setState(() {
+        _student = studentData; // 데이터 저장
+      });
+    } catch (e) {
+      print("Error loading student data: $e");
+      setState(() {
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    // 로딩 상태 확인
+    if (_student.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("학생 정보 로딩 중..."),
+          backgroundColor: Color(0xffF4F4F4),
+        ),
+        backgroundColor: Color(0xffF4F4F4),
+        body: Center(
+          child: CircularProgressIndicator(), // 로딩 인디케이터 표시
+        ),
+      );
+    }
 
-      body: Text("내정보페이지"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${_student['studentName']} 학생 인적 사항"),
+        backgroundColor: Color(0xffF4F4F4),
+      ),
+      backgroundColor: Color(0xffF4F4F4),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 15),
+                      if( _student['studentImg'] != null )
+              Image.network(
+                      "http://112.221.66.174:6892${_student['studentImg']}",
+                         width: 100,
+                         height: 100,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                           return Container(
+                        width: 100,
+                         height: 100,
+                           color: Colors.grey[300],
+                          child: Icon(Icons.image, color: Colors.grey[700]),
+                         );
+                            },
+                           ),
+                      Container(
+                        child:
+                        Text("프로필 사진"),
+                      ),
+                      SizedBox(height: 18),
+
+                      // 학생 이름
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("이 름", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text("${_student['studentName']}",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      // 생년월일
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("생년월일 ", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text(
+                                  "${_student['studentBirth'] ?? '미입력'}",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      // 학교, 학년 등
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("학교", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text(
+                                  "${_student['schoolName']}/${_student['grade']}학년/${_student['classNo']}반 / ${_student['studentNo']}번",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      // 성별
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("성별", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text(
+                                  "${_student['studentGender'] ?? '미입력' } ",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      // 연락처
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("핸드폰", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text(
+                                  "${_student['studentPhone'] ?? '미입력' }",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      // 보호자
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("보호자", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text("${_student['parentsName'] ?? '미입력'}",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("보호자 연락처",
+                                style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text(
+                                  "${_student['parentsPhone'] ?? '미입력' }",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("주소", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text(
+                                  "${_student['studentAddress'] ?? '미입력' }",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 15),
+
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("특이사항", style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Text("${_student['studentEtc'] ?? '미입력' }",
+                                  style: TextStyle(fontSize: 15)),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.grey)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 15),
+
+                      Row(
+                        children: [
+                          Container(
+                            width: 87,
+                            child: Text("메모", style: TextStyle(fontSize: 16),
+                                textAlign: TextAlign.center),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 14.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.all(12.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      border: Border.all(
+                                          color: Colors.grey.withOpacity(0.5)),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.1),
+                                          blurRadius: 8,
+                                          offset: Offset(0, 4),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Text(
+                                      "${_student['memo'] ?? '미입력'}",
+                                      style: TextStyle(fontSize: 18,
+                                          color: Colors.black.withOpacity(0.7)),
+                                    ),
+                                  ),
+
+                                  SizedBox(height: 15)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyPageUpdate(studentData: _student),
+                ),
+              ).then((updatedData) {
+                if (updatedData != null) {
+                  setState(() {
+                    _student = updatedData;
+                  });
+                }
+              });
+
+            },
+            icon: const Icon(Icons.update ),
+            label: const Text("수정하기"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xff515151), // 버튼 배경색 변경
+              foregroundColor: Colors.white, // 버튼 텍스트 색 변경
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
-  }
-}
+  }}
---
+++ lib/screen/myPageUpdate.dart
@@ -0,0 +1,229 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lastdance_f/model/student_model.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class MyPageUpdate extends StatefulWidget {
+  final Map<String, dynamic> studentData;
+
+  const MyPageUpdate({super.key, required this.studentData});
+
+  @override
+  State<MyPageUpdate> createState() => _MyPageUpdateState();
+}
+
+class _MyPageUpdateState extends State<MyPageUpdate> {
+  final StudentModel _studentModel = StudentModel();
+
+  late TextEditingController _nameController;
+  late TextEditingController _birthController;
+  late TextEditingController _phoneController;
+  late TextEditingController _addressController;
+  late TextEditingController _studentEtcController;
+  late TextEditingController _parentsNameController;
+  late TextEditingController _parentsPhoneController;
+  late TextEditingController _studentGenderController;
+
+  File? _selectedImage;
+  String? beforeImg;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.studentData['studentName'] ?? '');
+    _birthController = TextEditingController(text: widget.studentData['studentBirth'] ?? '');
+    _phoneController = TextEditingController(text: widget.studentData['studentPhone'] ?? '');
+    _addressController = TextEditingController(text: widget.studentData['studentAddress'] ?? '');
+    _studentEtcController = TextEditingController(text: widget.studentData['studentEtc'] ?? '');
+    _parentsNameController = TextEditingController(text: widget.studentData['parentsName'] ?? '');
+    _parentsPhoneController = TextEditingController(text: widget.studentData['parentsPhone'] ?? '');
+    _studentGenderController = TextEditingController(text: widget.studentData['studentGender'] ?? '');
+
+    if (widget.studentData['studentImg'] != null) {
+
+      beforeImg = widget.studentData['studentImg'];
+      print("여기");
+      print(beforeImg);
+      print("여기");
+
+    }
+  }
+
+  Future<void> _pickImage() async {
+    // 권한 확인
+    await _checkPermission(Permission.photos);
+
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null && pickedFile.path.isNotEmpty) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("이미지를 선택할 수 없습니다.")),
+      );
+    }
+  }
+
+  Future<void> _checkPermission(Permission permission) async {
+    PermissionStatus permissionStatus = await permission.status;
+    if (!permissionStatus.isGranted) {
+      permissionStatus = await permission.request();
+      if (!permissionStatus.isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("갤러리 접근 권한이 필요합니다.")),
+        );
+        openAppSettings();
+      }
+    }
+  }
+
+
+
+  Future<void> _saveChanges() async  {
+    String studentImgPath = _selectedImage != null ? _selectedImage!.path : '';
+    Map<String, dynamic> updatedData = {
+      'studentName': _nameController.text,
+      'studentBirth': _birthController.text,
+      'studentPhone': _phoneController.text,
+      'studentAddress': _addressController.text,
+      'studentEtc': _studentEtcController.text,
+      'parentsName': _parentsNameController.text,
+      'parentsPhone': _parentsPhoneController.text,
+      'studentGender': _studentGenderController.text,
+      'studentImg': studentImgPath,
+      'studentId': widget.studentData['studentId']
+    };
+
+    try {
+      final dio = Dio();
+      FormData formData = FormData.fromMap({
+        'studentName': updatedData['studentName'],
+        'studentBirth': updatedData['studentBirth'],
+        'studentPhone': updatedData['studentPhone'],
+        'studentAddress': updatedData['studentAddress'],
+        'studentEtc': updatedData['studentEtc'],
+        'parentsName': updatedData['parentsName'],
+        'parentsPhone': updatedData['parentsPhone'],
+        'studentGender': updatedData['studentGender'],
+        'studentId': updatedData['studentId'],
+        if (_selectedImage != null)
+          'studentImg': await MultipartFile.fromFile(_selectedImage!.path),
+      });
+
+      Response response = await dio.post(
+        'http://112.221.66.174:6892/api/students/register/${updatedData['studentId']}',
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("메모 업데이트 성공")),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("업데이트 실패")),
+        );
+      }
+    } catch (e) {
+      print("Error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("업데이트 중 오류가 발생했습니다.")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("학생 정보 수정"),
+        backgroundColor: const Color(0xffF4F4F4),
+      ),
+      backgroundColor: const Color(0xffF4F4F4),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                if (_selectedImage != null)
+                  Image.file(
+                    _selectedImage!,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  )
+                else if (beforeImg != null)
+                  Image.network(
+                    "http://112.221.66.174:6892$beforeImg",
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  )
+                else
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[300],
+                    child: Icon(Icons.image, color: Colors.grey[700]),
+                  ),
+                ElevatedButton(
+                  onPressed: _pickImage,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff515151),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  child: const Text("프로필 변경"),
+                ),
+              ],
+            ),
+            _buildTextField("이 름", _nameController),
+            _buildTextField("성별", _studentGenderController),
+            _buildTextField("생년월일", _birthController),
+            _buildTextField("핸드폰", _phoneController),
+            _buildTextField("주소", _addressController),
+            _buildTextField("보호자", _parentsNameController),
+            _buildTextField("보호자 번호", _parentsPhoneController),
+            _buildTextField("특이사항", _studentEtcController),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _saveChanges,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xff515151),
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              child: const Text("저장"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
