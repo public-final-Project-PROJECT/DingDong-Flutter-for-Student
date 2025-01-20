bc2013a7348e933b773e0e837ee06efa0c52093d
RJANG <msgpigeon@gmail.com>
2025-01-20 16:16:27 +0900

    .

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -29,30 +29,27 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  int _studentId = 0;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
   late Future<Map<String, dynamic>> classDetailsFuture;
-
-  String getServerURL() {
-    return kIsWeb
-        ? dotenv.env['FETCH_SERVER_URL2']!
-        : dotenv.env['FETCH_SERVER_URL']!;
-  }
 
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
   @override
   void initState() {
     super.initState();
+    _fetchStudentId(widget.student.classId);
     classDetailsFuture = _fetchClassDetails(widget.student.classId);
+    _initializeNotifications();
+  }
 
-    requestNotificationPermission();
+  void _initializeNotifications() {
+    _requestNotificationPermission();
     initNotification();
 
     _firebaseMessaging.getToken().then((token) {
-
       if (token != null) {
-        sendTokenToServer(token, 1);
+        _sendTokenToServer(token, _studentId);
       }
     });
 
@@ -61,23 +58,25 @@ class _HomeScreenState extends State<HomeScreen> {
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      // Handle background notification tap
     });
 
     FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
   }
 
   static Future<void> _backgroundMessageHandler(RemoteMessage message) async {
+    // Handle background notifications
   }
 
-  Future<void> requestNotificationPermission() async {
+  Future<void> _requestNotificationPermission() async {
     if (await Permission.notification.isDenied) {
       await Permission.notification.request();
     }
   }
 
-  Future<void> sendTokenToServer(String token, int studentId) async {
-    final url = Uri.parse("http://112.221.66.174:6892/fcm/register-token");
-
+  Future<void> _sendTokenToServer(String token, int studentId) async {
+    final url =
+        Uri.parse("http://112.221.66.174:6892/fcm/register-token");
     final body = jsonEncode({
       "token": token,
       "studentId": studentId,
@@ -93,7 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<Map<String, dynamic>> _fetchClassDetails(int classId) async {
     try {
       final dio = Dio();
-      String serverURL = getServerURL();
+      final serverURL = _getServerURL();
       final response = await dio.get('$serverURL/class/$classId');
 
       if (response.statusCode == 200) {
@@ -104,6 +103,30 @@ class _HomeScreenState extends State<HomeScreen> {
     } catch (e) {
       throw Exception("Error: ${e.toString()}");
     }
+  }
+
+  Future<void> _fetchStudentId(int classId) async {
+    try {
+      final dio = Dio();
+      final serverURL = _getServerURL();
+      final response = await dio.get(
+          '$serverURL/api/students/get/$classId/${widget.student.studentInfo.studentNo}');
+
+      if (response.statusCode == 200) {
+        final data = response.data;
+        _studentId = data is int ? data : int.tryParse(data.toString()) ?? 0;
+      } else {
+        throw Exception("Failed to fetch class details.");
+      }
+    } catch (e) {
+      throw Exception("Error: ${e.toString()}");
+    }
+  }
+
+  String _getServerURL() {
+    return kIsWeb
+        ? dotenv.env['FETCH_SERVER_URL2']!
+        : dotenv.env['FETCH_SERVER_URL']!;
   }
 
   final List<Widget> _pages = [
@@ -129,117 +152,94 @@ class _HomeScreenState extends State<HomeScreen> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         } else if (snapshot.hasData) {
-          final classDetails = snapshot.data!;
-          return Scaffold(
-            key: _scaffoldKey,
-            appBar: AppBar(
-              title: Text(
-                "${classDetails['classNickname']}",
-                style: const TextStyle(fontSize: 20),
-              ),
-              actions: [
-                IconButton(
-                  icon: Icon(Icons.notifications),
-                  onPressed: () => _scaffoldKey.currentState?.openEndDrawer(),
-                ),
-              ],
-            ),
-            drawer: Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  DrawerHeader(
-                    child: Text(
-                      '${classDetails['schoolName']} '
-                      '${classDetails['grade']}학년 '
-                      '${classDetails['classNo']}반 '
-                      '${widget.student.studentInfo.studentName}',
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                  ),
-                  ListTile(
-                    title: const Text('홈'),
-                    onTap: () {
-                      _onTapped(0);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('공지사항'),
-                    onTap: () {
-                      _onTapped(1);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('마이 페이지'),
-                    onTap: () {
-                      _onTapped(2);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('캘린더'),
-                    onTap: () {
-                      _onTapped(3);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: Row(
-                      children: const [
-                        Icon(
-                          Icons.table_restaurant_outlined,
-                          color: Colors.deepOrange,
-                          size: 30,
-                        ),
-                        SizedBox(width: 10),
-                        Text('우리반 좌석보기'),
-                      ],
-                    ),
-                    onTap: () {
-                      _onTapped(4);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: Row(
-                      children: const [
-                        Icon(
-                          Icons.how_to_vote_outlined,
-                          color: Colors.deepOrange,
-                          size: 30,
-                        ),
-                        SizedBox(width: 10),
-                        Text('학급 투표'),
-                      ],
-                    ),
-                    onTap: () {
-                      _onTapped(5);
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-            endDrawer: EndDrawerWidget(),
-            body: _pages[_selectedIndex],
-          );
+          return _buildScaffold(snapshot.data!);
         } else if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: Text(
-                "오류: ${snapshot.error}",
-                style: const TextStyle(fontSize: 18),
-              ),
-            ),
-          );
+          return _buildErrorScreen(snapshot.error);
         } else {
-          return const Center(
-            child: Text("오류"),
-          );
+          return const Center(child: Text("오류"));
         }
       },
+    );
+  }
+
+  Scaffold _buildScaffold(Map<String, dynamic> classDetails) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: _buildAppBar(classDetails['classNickname']),
+      drawer: _buildDrawer(classDetails),
+      endDrawer: const EndDrawerWidget(),
+      body: _pages[_selectedIndex],
+    );
+  }
+
+  AppBar _buildAppBar(String classNickname) {
+    return AppBar(
+      title: Text(
+        classNickname,
+        style: const TextStyle(fontSize: 20),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.notifications),
+          onPressed: () => _scaffoldKey.currentState?.openEndDrawer(),
+        ),
+      ],
+    );
+  }
+
+  Drawer _buildDrawer(Map<String, dynamic> classDetails) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          _buildDrawerHeader(classDetails),
+          _buildDrawerItem('홈', Icons.home, 0),
+          _buildDrawerItem('공지사항', Icons.announcement, 1),
+          _buildDrawerItem('마이 페이지', Icons.person, 2),
+          _buildDrawerItem('캘린더', Icons.calendar_today, 3),
+          _buildDrawerItem('우리반 좌석보기', Icons.table_restaurant_outlined, 4),
+          _buildDrawerItem('학급 투표', Icons.how_to_vote_outlined, 5),
+        ],
+      ),
+    );
+  }
+
+  DrawerHeader _buildDrawerHeader(Map<String, dynamic> classDetails) {
+    return DrawerHeader(
+      child: Text(
+        '${classDetails['schoolName']} '
+        '${classDetails['grade']}학년 '
+        '${classDetails['classNo']}반\n'
+        '${widget.student.studentInfo.studentName}',
+        style: const TextStyle(fontSize: 24),
+      ),
+    );
+  }
+
+  ListTile _buildDrawerItem(String title, IconData icon, int index) {
+    return ListTile(
+      title: Row(
+        children: [
+          Icon(icon, color: Colors.deepOrange, size: 30),
+          const SizedBox(width: 10),
+          Text(title),
+        ],
+      ),
+      onTap: () {
+        _onTapped(index);
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  Scaffold _buildErrorScreen(Object? error) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "오류: $error",
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
